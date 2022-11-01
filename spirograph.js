1ff85c1c6f8f2b010d772027af1742cf54906c36
gianmarco-todesco <gianmarco.todesco@gmail.com>
2022-11-01 16:57:16 +0100

    .

--- spirograph.js
+++ spirograph.js
@@ -1,6 +1,3 @@
-
-
-
 "use strict";
 
 function gcd(a,b) {
@@ -8,9 +5,17 @@ function gcd(a,b) {
     return a;
 }
 
+function getDist(a,b) { 
+    let dx = b.x-a.x, dy = b.y-a.y; 
+    return Math.sqrt(dx*dx+dy*dy); ù
+}
+
+
+// ----------------------------------------------------------------------------
+
 class GearBox {
     constructor(n1, n2, pDist = 0.7) {
-        this.toothWidth = 20;
+        this.toothWidth = 17;
         this.toothHeight = this.toothWidth * 0.4;
         this.toothFactor = this.toothWidth/(2*Math.PI);
         this.n1 = n1;
@@ -18,8 +23,7 @@ class GearBox {
         this._setN2(n2);
         this.phi = 0;
         this.psiOffset = 0;
-        this.c1 = [0,0];
-        this.c2 = [0,0];
+        this.c1 = {x:0, y:0};
         this.makePens();
         this.update();
     }
@@ -31,14 +35,10 @@ class GearBox {
     }
 
     setN2(n2) {
-        const { phi, n1, psiOffset } = this;
+        const { phi, n1 } = this;
         let delta =  phi * (1 - n1/this.n2) - phi*(1 - n1/n2);
         this._setN2(n2);
-        // note: use psiOffset instead of psiIntOffset
-        // the idea is to stabilize the orientation of gear2 while changing n2 
-        let angleUnit = 2*Math.PI/this.n2;
         this.psiOffset += delta;
-        //this.psiRoundedOffset = angleUnit * Math.floor(0.5 + this.psiOffset / angleUnit);
         this.update();        
     }
 
@@ -47,8 +47,13 @@ class GearBox {
         this.updatePens();
     }
 
+    setPhi(phi) {
+        this.phi = phi;
+        this.update();
+    }
+
     makePens() {
-        this.pens = [Math.PI, Math.PI + 2*Math.PI/5].map(theta => ({
+        this.pens = [Math.PI].map(theta => ({
             theta, pDist : 0.5, localPt : {x:0, y:0}, localEndPt : {x:0, y:0}, pt : { }
         }))
     }
@@ -57,7 +62,7 @@ class GearBox {
         let psiOffset = this.psiOffset;
         let psi = this.psi;
         let r2 = this.r2;
-        let cx = this.c1[0], cy = this.c1[1];
+        let cx = this.c1.x, cy = this.c1.y;
 
         this.pens.forEach(pen => {
             let localTheta = psiOffset + pen.theta;
@@ -77,17 +82,14 @@ class GearBox {
     update() {
         const { n1, n2, psiOffset, psiRoundedOffset, r1, r2, phi, psi, pDist } = this;
         this.psi = phi * (1 - n1/n2); //  * 2 * Math.PI / n2;
-        this.c1[0] = Math.cos(phi) * (r1-r2);
-        this.c1[1] = Math.sin(phi) * (r1-r2);
+        this.c1.x = Math.cos(phi) * (r1-r2);
+        this.c1.y = Math.sin(phi) * (r1-r2);
         
-        //let r = r2 * pDist;
-        //this.c2[0] = Math.cos(psi+Math.PI+psiOffset) * r;
-        //this.c2[1] = Math.sin(psi+Math.PI+psiOffset) * r;
         this.updatePens();
     }
 }
 
-function getDist(a,b) { let dx = b.x-a.x, dy = b.y-a.y; return Math.sqrt(dx*dx+dy*dy); }
+// ----------------------------------------------------------------------------
 
 class Trail {
     constructor(gearBox, penIndex) {
@@ -116,57 +118,43 @@ class Trail {
         pts.push(pt);
     }
 
+    recompute() {
+        const { pts, gearBox, pen } = this;
+        let oldPhi = gearBox.phi;
+        pts.forEach(p => {
+            gearBox.setPhi(p.phi);
+            p.x = pen.pt.x;
+            p.y = pen.pt.y;            
+        });
+        gearBox.setPhi(oldPhi);
+    }
+
     prune() {
         let phi = this.gearBox.phi;
         let maxAngle = Math.PI*2*this.gearBox.orbitCount*trailLength;
         const pts = this.pts;
         let i = 0;
-        while(i+1<pts.length && phi-pts[i+1].phi > maxAngle) i++;
-        if(i>0) pts.splice(0,i);
+        while(i+1<pts.length && phi-pts[i+1].phi >= maxAngle) i++;
+        if(i>0) {
+            // i+1>=pts.length || assert phi-pts[i+1].phi < maxAngle
+            // assert phi-maxAngle > pts[i].phi 
+            if(i+1<pts.length) {
+                let pt = pts[i];
+                pt.phi = phi - maxAngle;
+                let oldPhi = gearBox.phi;
+                gearBox.setPhi(pt.phi);
+                pt.x = this.pen.pt.x;
+                pt.y = this.pen.pt.y;
+                gearBox.setPhi(oldPhi);                                
+            }
+            
+            pts.splice(0,i);
+        }
     }
-
-
 }
 
-let canvas, ctx;
-const gearBox = new GearBox(120,70,0.7);
 
-/*
-const toothWidth = 20, toothHeight = 8;
-let n1 = 120, n2 = 70;
-let orbitCount = n1/gcd(n1,n2);
-let pDist = 0.7;
-*/
-let trailLength = 0; // as a fraction of the orbitLength
-
-/*
-let r1, r2;
-r1 = computeRadius(n1);
-r2 = computeRadius(n2);
-
-let phi=0, psi=0, psiOffset = 0; 
-let c0, c1, c2, pt;
-*/
-
-let speed = 0*20;
-let oldTime = performance.now();
-
-let trail = new Trail(gearBox,0);
-//let trailPts = [];
-//let trailMode = 0; // 0=niente coda, 1=coda, 2=traiettoria completa
-
-
-document.addEventListener('DOMContentLoaded', ()=>{
-
-    buildLayout();
-
-    ctx = canvas.getContext('2d');
-    
-    updateFields();
-
-    animate();
-
-})
+// ----------------------------------------------------------------------------
 
 function addSlider(container, vmin, vmax, value, cb) {
     let sld = document.createElement('input');
@@ -187,133 +175,47 @@ function addParagraph(container, text) {
     return p;
 }
 
-function buildLayout() {
-    let style = document.createElement('style');
-    style.innerHTML = styleContent;
-    document.head.appendChild(style);
-    let container = document.getElementById('container');
-    canvas = document.createElement('canvas');
-    container.appendChild(canvas);
+// ----------------------------------------------------------------------------
 
-    let sidePanel = document.createElement('div');
-    container.appendChild(sidePanel);
-    let infoPanel = document.createElement('div');
-    sidePanel.appendChild(infoPanel);
-    infoPanel.innerHTML = `
-    <p>N<sub>1</sub> = <span id="n2-fld">2</span></p>
-    <p>N<sub>2</sub> = <span id="n1-fld">2</span></p>
-    <p>Rapporto</sub> =             
-        <span class="fraction">
-            <span id="num-fld" class="num">1</span>
-            <span id="den-fld" class="den">2</span>
-        </span>
-    </p>
-    `;
-    let controlPanel = document.createElement('div');
-    sidePanel.appendChild(controlPanel);
-    addParagraph(controlPanel, "Numero denti ingranaggio interno");
-    addSlider(controlPanel, 35,100,gearBox.n2, v => { gearBox.setN2(v); trail.clear(); updateFields(); } )
-    addParagraph(controlPanel, "Distanza dal centro della penna");
-    addSlider(controlPanel, 1,100,Math.floor(0.5+gearBox.pDist*100), v=> gearBox.setPDist(v*0.01))
-    addParagraph(controlPanel, "Velocità");
-    addSlider(controlPanel, 0,100,speed, (v)=> {speed=v; return false;})
-    addParagraph(controlPanel, "Lunghezza scia");
-    addSlider(controlPanel, 0,100,trailLength*100, (v)=> setTrailLength(v*0.01))
+let canvas, ctx;
+const gearBox = new GearBox(120,70,0.7);
+let trailLength = 1.0; // as a fraction of the orbitLength
 
-    // see https://stackoverflow.com/questions/69490604/html-input-range-type-becomes-un-usable-by-drag-action-if-highlighted-in-chrome
-    document.querySelectorAll('input[type="range"]').forEach((input) => { 
-        input.addEventListener('mousedown',  () => window.getSelection().removeAllRanges());
-    });
-}
+let speed = 20;
+let oldTime = performance.now();
+
+let trail = new Trail(gearBox,0);
+
+// ----------------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    buildLayout();
+    ctx = canvas.getContext('2d');    
+    updateFields();
+    animate();
+})
+
+
 
 function animate() {
+    let time = performance.now();
+    let dt = (time-oldTime);
+    oldTime = time;
+    gearBox.phi += speed * dt * 0.00005;
+    gearBox.update();
+    trail.tick();
     redraw();
     requestAnimationFrame(animate);
 }
 
-/*
-function updateTrail() {
-    trailMode = 2;
-    trailPts = [];
-    let oldPhi = gearBox.phi;
-    let startPhi = gearBox.phi - Math.PI*2*gearBox.orbitCount * trailLength / 100.0;
-    gearBox.phi = startPhi;
-    gearBox.update();
-    trailPts.push(gearBox.c2);
-    const dphi = 0.1;
-    gearBox.phi += dphi;
-    while(gearBox.phi < oldPhi) {
-        gearBox.update();
-        trailPts.push(gearBox.c2);
-        gearBox.phi += dphi;
-    }
-    gearBox.phi = oldPhi;
-    gearBox.update();
-    trailPts.push(gearBox.c2);
-}
-*/
 
-function setTrailLength(v) {
-    trailLength = v;
-    // updateTrail();
-    
-}
-
-/*
-function setN2(_n2) {
-    trailPts = [];
-    n2 = _n2;
-    r1 = computeRadius(n1);
-    r2 = computeRadius(n2);
-    orbitCount = n1/gcd(n1,n2);
-    let delta = psi - computePsi(phi);
-    let angleUnit = 2*Math.PI/n2;
-    psiOffset += Math.floor(0.5 + delta/angleUnit);
-    update();
-    if(trailMode==2) updateTrail();
-    updateFields();
-}
-
-function setPDist(_pDist) {
-    pDist = Math.min(1, Math.max(0.2, _pDist));
-    if(trailMode == 2) updateTrail();
-    else if(trailMode == 1) pts = [];
-}
-
-function computePsi(phi) 
-{
-    return phi - phi * n1/n2 + psiOffset * 2*Math.PI/n2;    
-}
-*/
-
-document.addEventListener('keydown', (e) => {
-    console.log(e);
-    /*
-    if(e.key == "w") { setN2(n2+1); }
-    else if(e.key == "q") { setN2(n2-1); }
-    else if(e.key == "a") { setPDist(pDist - 0.1); }
-    else if(e.key == "s") { setPDist(pDist + 0.1);  }
-    else if(e.key == '1') { trailPts = []; trailMode = 0; }
-    else if(e.key == '2') { trailPts = []; trailMode = 1; }
-    else if(e.key == '3') { updateTrail(); trailMode = 2; }
-    else if(e.key == '+') { speed ++; }
-    else if(e.key == '-') { if(speed>0) speed --; }
-    */
-});
-
-/*
-function computeRadius(n) 
-{
-    return toothWidth*n/(2*Math.PI);
-}
-*/
+// ----------------------------------------------------------------------------
 
 function circlePath(x,y,r) {
     ctx.beginPath();
     ctx.moveTo(x+r,y);
     ctx.arc(x,y,r,0,2*Math.PI);
 }
-
 
 function createGearOutline(n) {
     const phi_i = 2*Math.PI/n;
@@ -409,62 +311,7 @@ function drawInGear() {
     });
 }
 
-function tick() {
-    let time = performance.now();
-    let dt = (time-oldTime);
-    oldTime = time;
-
-    gearBox.phi += speed * dt * 0.00005;
-    gearBox.update();
-    trail.tick();
-}
-
-/*
-function updateGearPositions() {
-
-    psi = computePsi(phi);
-    c1 = [Math.cos(phi) * (r1-r2) + c0[0], 
-        Math.sin(phi) * (r1-r2) + c0[1]];
-    c2 = [
-        Math.cos(psi+Math.PI) * r2 * pDist + c1[0], 
-        Math.sin(psi+Math.PI) * r2 * pDist + c1[1]
-    ];
-    /*
-    if(trailMode==1) {
-        trailPts.push(c2);
-        const maxPtsCount = 10000;
-        if(trailPts.length>maxPtsCount)
-            trailPts.splice(0, trailPts.length-maxPtsCount)    
-    }
-    * /
-}
-*/
-
-function redraw() {
-
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.save();
-    ctx.translate(canvas.width/2, canvas.height/2);
-
-    tick();
-    gearBox.update();
-     
-
-    drawOutGear();
-
-
-
-    // draw inner gear
-    ctx.save();    
-    ctx.translate(gearBox.c1[0], gearBox.c1[1]);
-    ctx.rotate(gearBox.psi);
-    drawInGear();
-    ctx.restore();
-
-
-    // draw trail.
+function drawTrail(trail) {
     if(trail.pts.length>2) {
         ctx.beginPath();
         ctx.moveTo(trail.pts[0].x, trail.pts[0].y);
@@ -476,11 +323,84 @@ function redraw() {
         ctx.lineWidth = 1;
         ctx.globalAlpha = 1;        
     }
-
-    ctx.restore();
-    // update(performance.now()*0.001);
 }
 
+function redraw() {
+
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.save();
+    ctx.translate(canvas.width/2, canvas.height/2);     
+
+    // draw outer gear
+    drawOutGear();
+
+    // draw inner gear
+    ctx.save();    
+    ctx.translate(gearBox.c1.x, gearBox.c1.y);
+    ctx.rotate(gearBox.psi);
+    drawInGear();
+    ctx.restore();
+
+
+    // draw trail.
+    drawTrail(trail);
+
+    ctx.restore();
+}
+
+// ----------------------------------------------------------------------------
+
+function buildLayout() {
+    let style = document.createElement('style');
+    style.innerHTML = styleContent;
+    document.head.appendChild(style);
+    let container = document.getElementById('container');
+    canvas = document.createElement('canvas');
+    container.appendChild(canvas);
+
+    let sidePanel = document.createElement('div');
+    container.appendChild(sidePanel);
+    let infoPanel = document.createElement('div');
+    infoPanel.classList.add("info-panel");
+    sidePanel.appendChild(infoPanel);
+    infoPanel.innerHTML = `
+    <p>N<sub>interno</sub> = <span id="n2-fld">2</span></p>
+    <p>N<sub>esterno</sub> = <span id="n1-fld">2</span></p>
+    <p>Rapporto</sub> =             
+        <span class="fraction">
+            <span id="num-fld" class="num">1</span>
+            <span id="den-fld" class="den">2</span>
+        </span>
+    </p>
+    `;
+    let controlPanel = document.createElement('div');
+    sidePanel.appendChild(controlPanel);
+    controlPanel.classList.add("control-panel");
+    addParagraph(controlPanel, "Numero denti ingranaggio interno");
+    addSlider(controlPanel, 35,100,gearBox.n2, v => { 
+        gearBox.setN2(v); 
+        trail.recompute(); 
+        updateFields(); 
+    } )
+    addParagraph(controlPanel, "Distanza dal centro della penna");
+    addSlider(controlPanel, 1,100,Math.floor(0.5+gearBox.pDist*100), v=> {
+        gearBox.setPDist(v*0.01);
+        trail.recompute();
+    });
+
+    addParagraph(controlPanel, "Velocità");
+    addSlider(controlPanel, 0,100,speed, (v)=> speed=v)
+
+    addParagraph(controlPanel, "Max. lunghezza scia");
+    addSlider(controlPanel, 0,100,trailLength*100, (v)=> trailLength = v*0.01 )
+
+    // see https://stackoverflow.com/questions/69490604/html-input-range-type-becomes-un-usable-by-drag-action-if-highlighted-in-chrome
+    document.querySelectorAll('input[type="range"]').forEach((input) => { 
+        input.addEventListener('mousedown',  () => window.getSelection().removeAllRanges());
+    });
+}
 
 function updateFields() {
     document.getElementById('n1-fld').innerHTML = gearBox.n1;
@@ -492,32 +412,23 @@ function updateFields() {
 
 
 const styleContent = `
-html, body {
-    overflow: hidden;
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    font-family: 'Lato', sans-serif;
-}
-canvas {
-    width:900px;
-    height:900px;
-}
 #container {
     display: flex;
     flex-direction: row;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
 }
-h1 {
-    font-size:50px;
+canvas {
+    width:750px;
+    height:750px;
+    margin-right:30px;
 }
-#info p {
+.info-panel {
     font-size:30px;
     font-weight:bold;
+    margin-bottom: 2em;
+
 }
-#info td { padding:10px;}
 .fraction {
     display:inline-flex;
     flex-direction:column;
@@ -529,7 +440,10 @@ h1 {
 .slider-container {
     font-size:20px;
 }
-
+.control-panel p {
+    margin-top:1em;
+    margin-bottom:0;
+}
 input[type=range] {
     width:220px;
 }
